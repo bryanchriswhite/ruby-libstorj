4fbdced3848f24517d42432c4edda9f0403ff3bd
Bryan White <bryanchriswhite@gmail.com>
2017-07-24 20:20:36 +0200

    add wip code

---
+++ test.rb
@@ -0,0 +1,52 @@
+require 'yaml'
+require_relative './libstorj.rb'
+include LibStorj
+
+def build_options(type_map)
+  options_yml = YAML.load_file "#{__dir__}/test/options.yml"
+  options_yml.to_a.map do |option_group|
+    group_name, options = option_group
+
+    option_type = type_map[group_name.to_sym]
+    member_field_hash = Hash[option_type.members.zip option_type.layout.fields]
+    option_instance = option_type.new
+
+    options.each do |option|
+      name, value = [option[0].to_sym, option[1]]
+
+      option_field = member_field_hash[name]
+
+      # TODO: check types and/or error handle
+      if value.nil?
+        option_instance[name] = FFI::Pointer::NULL
+      elsif option_field.is_a? FFI::StructLayout::Pointer
+        # Currently assuming pointers are actually string type
+        # (working around `ArgumentError: cannot set :string fields`)
+        pointer = FFI::MemoryPointer.from_string value
+        option_instance[name] = pointer
+      else
+        option_instance[name] = value
+      end
+
+    end
+
+    option_instance
+  end
+end
+
+def default_options
+  build_options bridge: StorjBridgeOptions_t,
+                encrypt: StorjEncryptOptions_t,
+                http: StorjHttpOptions_t,
+                log: StorjLogOptions_t
+end
+
+def test_env
+  # do stuff..
+  options = default_options
+  puts options.inspect
+
+  LibStorj.method(:init_env).call(*options)
+end
+
+test_env
