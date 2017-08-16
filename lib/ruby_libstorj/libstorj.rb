a2729bbda99565831e447f3b657e8475d26d82d0
Bryan White <bryanchriswhite@gmail.com>
2017-08-16 19:14:00 +0200

    remove LOAD_PATH scripting garbage from libstorj.rb

--- lib/ruby_libstorj/libstorj.rb
+++ lib/ruby_libstorj/libstorj.rb
@@ -1,4 +1,3 @@
-$:.unshift File.join(File.dirname(__FILE__), "..", "lib"), File.join(File.dirname(__FILE__), "..", "build", RUBY_VERSION) unless RUBY_PLATFORM =~ /java/
 require 'rubygems'
 require 'ffi'
 require 'date'
